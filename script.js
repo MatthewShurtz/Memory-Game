5b370e4d0c5f1641367b216eafea110e91b2d170
MatthewShurtz <59268254+MatthewShurtz@users.noreply.github.com>
2020-10-28 11:18:09 -0500

    will return to finish

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const antPicThree = {
   id: "three",
 };
 
+
 let questionMk = {
   img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/1920px-Question_mark_%28black%29.svg.png"};
 let clickedImg = document.querySelectorAll("img");
@@ -24,10 +25,15 @@ let attempts = 0;
 let numClicks = 0;
 let firstClick = [];
 let secondClick = [];
+let storedClicks = []
+let foundPics = [];
+// let imgAr = Array.from(clickedImg);
 
-cardReset = () => {
-        document.querySelectorAll("img").src = questionMk.img;
-      }
+
+// setQuestionPic = () => {
+//   img.src = questionMk.img;
+//   img.id = ""
+// };
 
 clickedImg.forEach((img) => {
   img.addEventListener("click", () => {
@@ -40,20 +46,23 @@ clickedImg.forEach((img) => {
       secondClick.push(randomPicAndId.id);
       if (firstClick[0] === secondClick[0]) {
         antPics.splice(antPics.indexOf(randomPicAndId), 1)
-        firstClick.shift(randomPicAndId.id);
-        secondClick.shift(randomPicAndId.id);
+        foundPics.push(firstClick[0]);
+        firstClick.shift();
+        secondClick.shift();
         alert("match");
       } else {
           alert("try again")
           if (firstClick[0] != secondClick[0]) {
-            firstClick.shift(randomPicAndId.id);
-            secondClick.shift(randomPicAndId.id);
-            clickedImg.forEach(cardReset);
+            firstClick.shift();
+            secondClick.shift();
           }
         }
-      } 
-    console.log(firstClick);
-    console.log(secondClick);
+      }
+    console.log(clickedImg[0]);  
+    console.log(randomPicAndId);  
+    console.log(clickedImg);
+    console.log(storedClicks);
     console.log(antPics);
+    console.log(foundPics);
   });
 });
